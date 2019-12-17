916c2d80cd268f0c078ed4746837adc46f81068c
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-17 15:24:56 +0200

    Knight Game solution

--- Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
+++ Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
@@ -15,22 +15,98 @@ namespace _7._Knight_Game
             {
                 matrix[row] = Console.ReadLine().ToCharArray().Where(x => x != ' ').ToArray();
             }
+            int knightCount = 0;
+            int maxAttacks = 0;
+            int killerRow = 0;
+            int killerCol = 0;
 
-            for (int row = 0; row < matrix.Length; row++)
+            while (true)
             {
-                for (int col = 0; col < matrix[row].Length; col++)
-                {
-                    if (matrix[row][col] == 'K')
-                    {
+                maxAttacks = 0;
 
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        int currentKnightsAttacks = 0;
+
+                        if (matrix[row][col] == 'K')
+                        {
+                            //down and right -> row + 2 , col + 1
+                            if (IsInside(matrix, row + 2, col + 1) && matrix[row + 2][col + 1] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            // down and left -> row + 2 , col - 1
+                            if (IsInside(matrix, row + 2, col - 1) && matrix[row + 2][col - 1] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            //up and right -> row - 2 , col + 1
+                            if (IsInside(matrix, row - 2, col + 1) && matrix[row - 2][col + 1] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            //up and left -> row - 2 , col - 1
+                            if (IsInside(matrix, row - 2, col - 1) && matrix[row - 2][col - 1] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            // left and up -> col + 2 , row  - 1
+                            if (IsInside(matrix, row - 1, col - 2) && matrix[row - 1][col - 2] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            // left and down -> col + 2 , row  + 1
+                            if (IsInside(matrix, row + 1, col - 2) && matrix[row + 1][col - 2] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            // right and up -> col + 2 , row  - 1
+                            if (IsInside(matrix, row - 1, col + 2) && matrix[row - 1][col + 2] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+
+                            // right and down -> col + 2 , row  + 1
+                            if (IsInside(matrix, row + 1, col + 2) && matrix[row + 1][col + 2] == 'K')
+                            {
+                                currentKnightsAttacks++;
+                            }
+                        }
+
+                        if (currentKnightsAttacks > maxAttacks)
+                        {
+                            maxAttacks = currentKnightsAttacks;
+                            killerRow = row;
+                            killerCol = col;
+                        }
                     }
                 }
-            }
 
-            foreach (var row in matrix)
-            {
-                Console.WriteLine($"{string.Join(" ", row)} ");
+                if (maxAttacks > 0)
+                {
+                    matrix[killerRow][killerCol] = '0';
+                    knightCount++;
+                }
+                else
+                {
+                    Console.WriteLine(knightCount);
+                    break;
+                }
             }
+        }
+
+        private static bool IsInside(char[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length &&
+           col >= 0 && col < matrix[row].Length;
         }
     }
 }
