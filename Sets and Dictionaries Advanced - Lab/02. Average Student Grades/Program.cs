992fd85f643732e43447d34b48e6352fb452225d
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-22 21:22:01 +0200

    Average Student Grades - 60/100

---
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> records = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string studentName = input[0];
+                double studentGrade = double.Parse(input[1]);
+
+                if (!records.ContainsKey(studentName))
+                {
+                    records.Add(studentName, new List<double>());
+                }
+
+                records[studentName].Add(studentGrade);
+            }
+
+            foreach (var student in records.OrderBy(x=>-x.Value.Average()))
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(x=>x.ToString("F2")))} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
