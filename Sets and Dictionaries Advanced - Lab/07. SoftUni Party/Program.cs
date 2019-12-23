6d6175007f53036a02788dcf2a5429f2a40267b1
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-23 14:25:04 +0200

    SoftUni Party

---
+++ Sets and Dictionaries Advanced - Lab/07. SoftUni Party/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> regularReservations = new HashSet<string>();
+            HashSet<string> VipReservations = new HashSet<string>();
+
+            bool reservation = true;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                if (command == "PARTY")
+                {
+                    reservation = false;
+                }
+
+                if (reservation && command.Length == 8)
+                {
+                    if (char.IsDigit(command[0]))
+                    {
+                        VipReservations.Add(command);
+                    }
+                    else
+                    {
+                        regularReservations.Add(command);
+                    }
+                }
+                else
+                {
+                    VipReservations.Remove(command);
+                    regularReservations.Remove(command);
+                }
+            }
+
+            Console.WriteLine(VipReservations.Count + regularReservations.Count);
+
+            foreach (var guest in VipReservations)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (var guest in regularReservations)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
