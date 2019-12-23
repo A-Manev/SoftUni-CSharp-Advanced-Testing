ce7b04ea097e59d4cbc2955dfff76ce31af09416
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-23 21:09:24 +0200

    Periodic Table

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    chemicalElements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicalElements));
+        }
+    }
+}
