425b1d3def4aff1fda23aafe7c4b9d2efe7e9c8e
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-29 22:06:08 +0200

    01. Sort Even Numbers

---
+++ Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
