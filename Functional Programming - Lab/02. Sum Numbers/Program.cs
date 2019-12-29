1533cfce874bfd6f09b0a558bd7344480f2719a4
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-29 22:17:47 +0200

    02. Sum Numbers

---
+++ Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
