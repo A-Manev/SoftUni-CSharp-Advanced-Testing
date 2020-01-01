bfedaafd77cbf4084bd681c92dff5f75aded490f
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-01 23:27:05 +0200

    Count Uppercase Words

---
+++ Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isUpper = UppercaseString;
+
+            foreach (var word in input)
+            {
+                if (isUpper(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+
+        static bool UppercaseString(string inputString)
+        {
+            return char.IsUpper(inputString[0]) ? true : false;
+
+            //if (char.IsUpper(inputString[0]))
+            //{
+            //    return true;
+            //}
+
+            //return false;
+        }
+    }
+}
