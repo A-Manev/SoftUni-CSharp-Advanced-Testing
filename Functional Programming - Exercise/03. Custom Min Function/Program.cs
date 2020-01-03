28ce55799fb6bde2e98678208157f2bea64b9ce5
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-03 11:44:06 +0200

    03. Custom Min Function

---
+++ Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int[], int> func = x => x.Min();
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(func(numbers));
+        }
+    }
+}
