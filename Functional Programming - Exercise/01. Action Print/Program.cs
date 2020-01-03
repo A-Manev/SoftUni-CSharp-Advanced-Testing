0a9c61f8a84b11c00b10b6d7c287f5856b738266
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-03 11:41:58 +0200

    01. Action Print

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ActionPrint
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<string[]> print = items => Console.WriteLine(string.Join(Environment.NewLine, items));
+
+            string[] input = Console.ReadLine()
+                .Split(new[] {' '},StringSplitOptions.RemoveEmptyEntries);
+
+            print(input);
+        }
+    }
+}
