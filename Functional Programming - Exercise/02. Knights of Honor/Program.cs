e970948d81116247d74c85268bacfe25950b7612
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-03 11:43:16 +0200

    02. Knights of Honor

---
+++ Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Action<string[]> action = x => Console.WriteLine($"Sir {string.Join(Environment.NewLine, x)}");
+
+            //string[] input = Console.ReadLine()
+            //    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //action(input);
+
+            Action<string> print = item => Console.WriteLine("Sir " + item);
+
+            Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(print);
+
+            //foreach (var item in input)
+            //{
+            //    Console.WriteLine("Sir " + item);
+            //}
+        }
+    }
+}
