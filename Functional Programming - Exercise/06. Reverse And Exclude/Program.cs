4f39fbf4f7eb88cdc99eb045659ebaed11cc318f
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-04 14:13:27 +0200

    06. Reverse And Exclude

---
+++ Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int divider = int.Parse(Console.ReadLine());
+
+            Predicate<int> filter = x => x % divider != 0;
+            Func<int, bool> filterFunc = x => filter(x);
+
+            input = input.Where(filterFunc).ToList();
+            input.Reverse();
+            print(input);
+        }
+    }
+}
