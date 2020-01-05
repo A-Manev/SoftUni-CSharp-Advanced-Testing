66cf7dec606054ae1e47f688daa8c043de0f9da2
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-05 19:17:51 +0200

    07. Predicate For Names

---
+++ Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            int lenght = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, bool> func = x => x.Length <= lenght;
+
+            names = names.Where(x => func(x)).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
